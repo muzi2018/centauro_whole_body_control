e94b4f4426b02552e7f1d0ca7f82788d955f0d3a
Weijie Wang <duangsir518@163.com>
2024-05-23 16:26:05 +0200

    read data
    from output.txt

--- ocs2_centauro_ros/src/LeggedRobotPoseCommandNode.cpp
+++ ocs2_centauro_ros/src/LeggedRobotPoseCommandNode.cpp
@@ -43,18 +43,34 @@ Additional modifications and contributions by Ioannis Dadiotis:
 #include <ros/service.h>
 #include <std_srvs/Empty.h>
 #include <xbot_msgs/JointState.h>
-
+#include <fstream>
+#include <iostream>
+#include <string>
 using namespace ocs2;
-
+using namespace std;
 
 scalar_t targetDisplacementVelocity;
 scalar_t targetRotationVelocity;
 scalar_t comHeight;
 vector_t defaultJointState;
 vector_t desireJointState;
+vector_t desirepose;
 bool iii = true;
 
 bool arm_rl_bool = false;
+
+std::vector<double> parseDoubles(const std::string& line) {
+    std::vector<double> doubles;
+    std::istringstream iss(line);
+    double value;
+
+    while (iss >> value) {
+        doubles.push_back(value);
+    }
+
+    return doubles;
+}
+
 
 bool arm_rl_tracking(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
 {
@@ -71,14 +87,14 @@ TargetTrajectories jointRefToTargetTrajectories(const SystemObservation& observa
   // std::cout << "*********observation.state.size*********" << std::endl;
   const vector_t currentPose = observation.state.segment<6>(6);
   // target reaching duration
-  const scalar_t targetReachingTime = observation.time + 10 ;
+  const scalar_t targetReachingTime = observation.time + 1 ;
   // desired time trajectory
   const scalar_array_t timeTrajectory{observation.time, targetReachingTime};
   // desired state trajectory
   // 6 base , 6 left arm, 6 right arm, 1 grippers + 2 = 13 +2
   vector_array_t stateTrajectory(2, vector_t::Zero(observation.state.size()));
   stateTrajectory[0] << vector_t::Zero(6), currentPose, defaultJointState;
-  stateTrajectory[1] << vector_t::Zero(6), currentPose, desireJointState;
+  stateTrajectory[1] << vector_t::Zero(6), desirepose, desireJointState;
 
   // desired input trajectory (just right dimensions, they are not used)
   const vector_array_t inputTrajectory(2, vector_t::Zero(observation.input.size()));
@@ -91,6 +107,10 @@ TargetTrajectories jointRefToTargetTrajectories(const SystemObservation& observa
     {
       std::cout << "desireJointState[" << i << "] = " << desireJointState[i] << std::endl;
     }
+    for (size_t i = 0; i < observation.state.size(); i++)
+    {
+      std::cout << "state[" << i << "] = " << observation.state[i] << std::endl;
+    }
     iii = false;
   }
   return {timeTrajectory, stateTrajectory, inputTrajectory};
@@ -99,7 +119,6 @@ TargetTrajectories jointRefToTargetTrajectories(const SystemObservation& observa
 
 int main(int argc, char* argv[]) {
   const std::string robotName = "legged_robot";
-
   // Initialize ros node
   ::ros::init(argc, argv, robotName + "_target");
   ::ros::NodeHandle nodeHandle;
@@ -155,12 +174,58 @@ int main(int argc, char* argv[]) {
   targetTrajectoriesPublisherPtr_.reset(new TargetTrajectoriesRosPublisher(nodeHandle, robotName));  
   ros::ServiceServer service = nodeHandle.advertiseService("arm_rl", arm_rl_tracking);
 
-  desireJointState = defaultJointState;
-  desireJointState[25] = 0.4; desireJointState[26] = 0.35; desireJointState[27] = 0.25; 
-  desireJointState[28] = -2.2; desireJointState[29] = 0.0; desireJointState[30] = -0.7;
 
-  desireJointState[31] = 0.36; desireJointState[32] = -0.73; desireJointState[33] = -0.75; 
-  desireJointState[34] = -1.4; desireJointState[35] = -0.24; desireJointState[36] = -0.14;
+
+
+  ifstream file("/home/wang/catkin_ws_1/src/centauro_whole_body_control/ocs2_centauro_ros/data/output.txt");
+  if (!file) {
+      cerr << "Unable to open file!" << endl;
+  }
+  std::vector<std::string> lines;
+  std::string line;
+  if (file.is_open()) {
+      while (std::getline(file, line)) {
+          lines.push_back(line);
+      }
+      file.close();
+  } else {
+      std::cerr << "Unable to open file" << std::endl;
+  }
+    std::cout << "lines.size() = " << lines.size() << std::endl; 
+
+
+  std::vector<std::vector<double>> doubleData;
+  for (const auto& line : lines) {
+      doubleData.push_back(parseDoubles(line));
+  }
+
+
+    std::cout << "doubleData.size() = " << doubleData.size() << std::endl; 
+    // std::cout << "doubleData[0].size() = " << doubleData[0].size() << std::endl;
+
+
+
+// 6 base , 6 left arm, 6 right arm, 1 grippers + 2 = 13 +2
+  desirepose.resize(6);
+  desirepose[0] = 0.07032587379217148; desirepose[1] = 0.5; desirepose[2] = 0.806497;
+  desirepose[3] = 0.19085979461669922; desirepose[4] = -0.20000000298023224; desirepose[5] = -0.5;
+
+  desireJointState = defaultJointState;
+    //left arm
+  desireJointState[25] = 0.5590618252754211; desireJointState[26] = 0.2764955163002014; desireJointState[27] = 0.30472493171691895; 
+  desireJointState[28] = -2.2533223628997803; desireJointState[29] = -0.06460489332675934; desireJointState[30] = -0.6942902207374573;
+  //right arm
+  desireJointState[31] = -0.19925080239772797; desireJointState[32] = -0.013799999840557575; desireJointState[33] = 0.009923404082655907; 
+  desireJointState[34] = -2.4793999195098877; desireJointState[35] = 0.09538839012384415; desireJointState[36] = 1.5155999660491943;
+  {
+  // //left arm
+  // desireJointState[25] = 0.4; desireJointState[26] = 0.35; desireJointState[27] = 0.25; 
+  // desireJointState[28] = -2.2; desireJointState[29] = 0.0; desireJointState[30] = -0.7;
+  // //right arm
+  // desireJointState[31] = 0.36; desireJointState[32] = -0.73; desireJointState[33] = -0.75; 
+  // desireJointState[34] = -1.4; desireJointState[35] = -0.24; desireJointState[36] = -0.14;
+  }
+
 
   ros::Rate r(100);
   while (ros::ok() && ros::master::check()) {
@@ -170,10 +235,7 @@ int main(int argc, char* argv[]) {
             ros::spinOnce();
             r.sleep();
         }
-
-
         ::ros::spinOnce();
-      
         SystemObservation observation;
         {
           std::lock_guard<std::mutex> lock(latestObservationMutex_);
