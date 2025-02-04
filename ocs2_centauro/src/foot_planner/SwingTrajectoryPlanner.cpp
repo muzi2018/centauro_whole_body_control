69d2c1bf1d5f8683f252752a05a91684c9c10c95
Weijie Wang <duangsir518@163.com>
2025-02-04 11:26:11 +0100

    change
    #decription
    modify the error effect for long and lateral step

--- ocs2_centauro/src/foot_planner/SwingTrajectoryPlanner.cpp
+++ ocs2_centauro/src/foot_planner/SwingTrajectoryPlanner.cpp
@@ -163,12 +163,12 @@ void SwingTrajectoryPlanner::update(const ModeSchedule& modeSchedule, scalar_t i
   auto longStepLength = this->getConfig().longStepLength;
   auto lateralStepLength = this->getConfig().lateralStepLength;
   
-  if (std::abs(x_e) < 0.1)
-  {
-    /* code */
-    longStepLength = 0;
-    lateralStepLength = 0;
-  }
+  // if (std::abs(x_e) < 0.1)
+  // {
+  //   /* code */
+  //   longStepLength = 0;
+  //   lateralStepLength = 0;
+  // }
   
 
   feet_array_t<scalar_array_t> targetEePosition = currentEePosition;
